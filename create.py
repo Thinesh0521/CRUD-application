4f61e3848b6ab111a1fd692026307a067a92d059
Thinesh0521 <93920209+Thinesh0521@users.noreply.github.com>
2021-11-15 02:58:07 +0000

    Update create.py

--- create.py
+++ create.py
@@ -1,5 +1,4 @@
 from application import db
-#Using the command "py create.py" in the terminal, the tables
-# in the database will upload automatically using SQLAlchemy.
+#Using the command "py create.py" in the terminal, the tables in the database will upload automatically using SQLAlchemy.
 db.drop_all()
 db.create_all()
